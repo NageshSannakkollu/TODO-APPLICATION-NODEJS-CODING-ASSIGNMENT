3ec534760943882893531f5fc6987e61a30da56c
NageshSannakkollu <122163329+NageshSannakkollu@users.noreply.github.com>
2024-01-17 05:49:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-
 const addDays = require("date-fns");
 const format = require("date-fns/format");
-
 const path = require("path");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-
 const databasePath = path.join(__dirname, "todoApplication.db");
 let database = null;
-
 const initializeDBAndServer = async () => {
   try {
     database = await open({
@@ -64,57 +58,70 @@ const hasTodoSearch = (requestQuery) => {
   return requestQuery.search_q !== undefined;
 };
 
-const getUpdatedData = (eachData) => ({
-  id: eachData.id,
-  todo: eachData.todo,
-  priority: eachData.priority,
-  status: eachData.status,
-  category: eachData.category,
-  dueDate: eachData.due_date,
-});
+const hasTodoDueDate = (requestQuery) => {
+  return requestQuery.dueDate !== undefined;
+};
 
 app.get("/todos/", async (request, response) => {
   const { status, priority, category, dueDate, search_q = "" } = request.query;
-  let getTodosQuery = "";
 
+  let getTodosQuery = "";
+  let todosStatus = "";
   switch (true) {
-    case hasPriorityAndStatus(request.body):
+    case hasPriorityAndStatus(request.query):
+      todosStatus = "Todo Priority and Status";
       getTodosQuery = `
-        SELECT * FROM todo WHERE todo LIKE '%${search_q}%' AND priority='${priority}' AND status='${status}';`;
+        SELECT id,todo,priority,status,category,due_date AS dueDate FROM todo WHERE todo LIKE '%${search_q}%' AND priority='${priority}' AND status='${status}';`;
       break;
     case hasCategoryAndStatus(request.query):
+      todosStatus = "Todo Category and Status";
       getTodosQuery = `
-        SELECT * FROM todo WHERE todo LIKE '%${search_q}%' AND category='${category}' AND status='${status}';`;
+        SELECT id,todo,priority,status,category,due_date AS dueDate FROM todo WHERE todo LIKE '%${search_q}%' AND category='${category}' AND status='${status}';`;
       break;
     case hasCategoryAndPriority(request.query):
+      todosStatus = "Todo Category and Priority";
       getTodosQuery = `
-        SELECT * FROM todo WHERE todo LIKE '%${search_q}%' AND category='${category}' AND priority='${priority}';`;
+        SELECT id,todo,priority,status,category,due_date AS dueDate FROM todo WHERE todo LIKE '%${search_q}%' AND category='${category}' AND priority='${priority}';`;
       break;
     case hasStatus(request.query):
+      todosStatus = "Todo Status";
       getTodosQuery = `
-        SELECT * FROM todo WHERE todo LIKE '%${search_q}%' AND status='${status}';`;
+        SELECT id,todo,priority,status,category,due_date AS dueDate FROM todo WHERE todo LIKE '%${search_q}%' AND status='${status}';`;
       break;
     case hasPriority(request.query):
+      todosStatus = "Todo Priority";
       getTodosQuery = `
-        SELECT * FROM todo WHERE todo LIKE '%${search_q}%' AND priority='${priority}';`;
+        SELECT id,todo,priority,status,category,due_date AS dueDate FROM todo WHERE todo LIKE '%${search_q}%' AND priority='${priority}';`;
       break;
     case hasCategory(request.query):
+      todosStatus = "Todo Category";
       getTodosQuery = `
-        SELECT * FROM todo WHERE todo LIKE '%${search_q}%' AND category='${category}';`;
+        SELECT id,todo,priority,status,category,due_date AS dueDate FROM todo WHERE todo LIKE '%${search_q}%' AND category='${category}';`;
       break;
     case hasTodoSearch(request.query):
-      getTodosQuery = `SELECT * FROM todo WHERE todo LIKE '%${search_q}%';`;
+      //console.log(request.query);
+      getTodosQuery = `SELECT id,todo,priority,status,category,due_date AS dueDate FROM todo WHERE todo LIKE '%${search_q}%';`;
       break;
+    case hasTodoDueDate(request.query):
+      todosStatus = "Due Date";
+      getTodosQuery = `SELECT id,todo,priority,status,category,due_date AS dueDate FROM todo WHERE due_date='${dueDate}';`;
   }
-  data = await database.get(getTodosQuery);
-  response.send(data.map((eachData) => getUpdatedData(eachData)));
+  data = await database.all(getTodosQuery);
+  console.log(data.length);
+
+  if (data.length === 0) {
+    response.status(400);
+    response.send(`Invalid ${todosStatus}`);
+  } else {
+    response.send(data);
+  }
 });
 
 //GET SPECIFIED TODO
 
 app.get("/todos/:todoId/", async (request, response) => {
   const { todoId } = request.params;
-  const getSpecificTodo = `SELECT * FROM todo WHERE id=${todoId};`;
+  const getSpecificTodo = `SELECT id,todo,priority,status,category,due_date AS dueDate FROM todo WHERE id=${todoId};`;
   const getTodoResponse = await database.get(getSpecificTodo);
   response.send(getTodoResponse);
 });
@@ -130,83 +137,100 @@ app.get("/agenda/", async (request, response) => {
   const dueDateChecking = checkDueDate(request.query);
   // console.log(dueDateChecking);
   const formattedDate = format(new Date(date), "yyyy-MM-dd");
-  console.log(formattedDate);
+  console.log(typeof formattedDate);
   if (dueDateChecking === true) {
-    const getDueDateQuery = `SELECT * FROM todo WHERE due_date='${formattedDate}';`;
+    const getDueDateQuery = `SELECT * FROM todo WHERE due_date LIKE '${formattedDate}';`;
     const dateResponse = await database.get(getDueDateQuery);
-    response.send(dateResponse);
+    console.log(dateResponse);
+    if (dateResponse === undefined) {
+      response.status(400);
+      response.send("Invalid Due Date");
+    } else {
+      response.send(dateResponse);
+    }
   }
 });
 
-//create a new todo
-
 app.post("/todos/", async (request, response) => {
-  const todoItemDetails = request.body;
-  const { id, todo, priority, status, category, dueDate } = todoItemDetails;
-  const createNewTodoQuery = `INSERT INTO todo VALUES (${id},'${todo}','${priority}','${status}','${category}','${dueDate}');`;
-  const createTodoResponse = await database.run(createNewTodoQuery);
-  const todoId = createTodoResponse.lastID;
-  response.send("Todo Successfully Added");
+  const { id, todo, priority, status, category, dueDate } = request.body;
+  let createTodoColumn = null;
+  console.log(dueDate);
+  switch (true) {
+    case status === undefined:
+      createTodoColumn = "Todo Status";
+      break;
+    case priority === undefined:
+      createTodoColumn = "Todo Priority";
+      break;
+    case category === undefined:
+      createTodoColumn = "Todo Category";
+      break;
+    case dueDate === undefined:
+      createTodoColumn = "Due Date";
+      break;
+  }
+
+  if (createTodoColumn === null) {
+    const updateTodoQuery = `
+    INSERT INTO 
+    todo (id,todo,priority,status,category,due_date)
+    VALUES (
+    ${id},'${todo}','${priority}','${status}','${category}','${dueDate}');`;
+    await database.run(updateTodoQuery);
+    response.send("Todo Successfully Added");
+  } else {
+    response.status(400);
+    response.send(`Invalid ${createTodoColumn}`);
+  }
 });
-
-//UPDATE SPECIFIC TODO
-
-const hasStatusProperty = (requestBody) => {
-  return requestBody.status !== undefined;
-};
-
-const hasPriorityProperty = (requestBody) => {
-  return requestBody.priority !== undefined;
-};
-
-const hasTodoProperty = (requestBody) => {
-  return requestBody.todo !== undefined;
-};
-
-const hasCategoryProperty = (requestBody) => {
-  return requestBody.category !== undefined;
-};
-
-const hasDueDateProperty = (requestBody) => {
-  return requestBody.dueDate !== undefined;
-};
 
 app.put("/todos/:todoId/", async (request, response) => {
   const { todoId } = request.params;
-  const { status, priority, todo, category, dueDate } = request.body;
-  let getUpdateTodo = "";
-  let data = "";
+  console.log(todoId);
+  const requestBody = request.body;
+  let updatedRequestColumn = null;
+
   switch (true) {
-    case hasStatusProperty(request.body):
-      getUpdateTodo = `UPDATE todo SET status='${status}' WHERE id=${todoId};`;
-      data = "Status";
+    case requestBody.status !== undefined:
+      updatedRequestColumn = "Status";
       break;
-    case hasPriorityProperty(request.body):
-      getUpdateTodo = `UPDATE todo SET priority='${priority}' WHERE id=${todoId};`;
-      data = "Priority";
+    case requestBody.priority !== undefined:
+      updatedRequestColumn = "Priority";
       break;
-    case hasTodoProperty(request.body):
-      getUpdateTodo = `UPDATE todo SET todo='${todo}' WHERE id=${todoId};`;
-      data = "Todo";
+    case requestBody.category !== undefined:
+      updatedRequestColumn = "Category";
       break;
-    case hasCategoryProperty(request.body):
-      getUpdateTodo = `UPDATE todo SET category='${category}' WHERE id=${todoId};`;
-      data = "Category";
+    case requestBody.dueDate !== undefined:
+      updatedRequestColumn = "Due Date";
       break;
-    case hasDueDateProperty(request.body):
-      getUpdateTodo = `UPDATE todo SET due_date='${dueDate}' WHERE id=${todoId};`;
-      data = "Due Date";
+    case requestBody.todo !== undefined:
+      updatedRequestColumn = "Todo";
       break;
   }
-  const updateResponse = await database.run(getUpdateTodo);
-  response.send(`${data} Updated`);
-});
 
-//DELETE SPECIFIC TODO
+  const previousTodoQuery = `SELECT * FROM todo WHERE id=${todoId};`;
+  const previousTodo = await database.get(previousTodoQuery);
+  console.log(previousTodo);
+  const { todo, status, priority, category, dueDate } = previousTodo;
+  const updatedTodoQuery = `
+  UPDATE 
+  todo 
+  SET 
+      todo='${todo}',
+      status='${status}',
+      priority='${priority}',
+      category='${category}',
+      due_date='${dueDate}' 
+      WHERE 
+      id=${todoId};`;
+  await database.run(updatedTodoQuery);
+  response.send(`${updatedRequestColumn} Updated`);
+});
 
 app.delete("/todos/:todoId/", async (request, response) => {
   const { todoId } = request.params;
-  const deleteQuery = `SELECT * FROM todo WHERE id=${todoId};`;
+  const deleteQuery = `
+    DELETE FROM todo WHERE id=${todoId};`;
   await database.run(deleteQuery);
   response.send("Todo Deleted");
 });
